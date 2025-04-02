661572e4103737ecf6505ebba20b20c79121f91b
Yusef Tohamy <tuo13985@temple.edu>
2025-04-02 15:20:50 -0400

    Updated code

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -2,43 +2,37 @@ package edu.temple.coroutineconversion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
-    //TODO (Refactor to replace Thread code with coroutines)
+  //TODO (Refactor to replace Thread code with coroutines)
+  private val cakeImageView: ImageView by lazy {
+    findViewById(R.id.imageView)
+  }
 
-    private val cakeImageView: ImageView by lazy {
-        findViewById(R.id.imageView)
-    }
+  private val currentTextView: TextView by lazy {
+    findViewById(R.id.currentTextView)
+  }
 
-    private val currentTextView: TextView by lazy {
-        findViewById(R.id.currentTextView)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
 
-    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
-
-        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
-        cakeImageView.alpha = it.what / 100f
-        true
-    })
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
-                }
-            }.start()
+    findViewById<Button>(R.id.revealButton).setOnClickListener {
+      lifecycleScope.launch {
+        repeat(100) {
+          currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it)
+          cakeImageView.alpha = it / 100f
+          delay(40)
         }
+      }
     }
+  }
 }
